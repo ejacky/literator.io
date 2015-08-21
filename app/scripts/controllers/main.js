c1f5647d781f42a5eee38ea7de8f4e75490c53ca
bobrosoft <bobrosoft@yandex.ru>
2015-08-21 17:06:31 +0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name literatorioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the literatorioApp
+ */
+angular.module('literatorioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
