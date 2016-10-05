cd85fc5b97afc7ed35d6d407e423401ba3671b0e
bobrosoft <bobrosoft@yandex.ru>
2016-10-05 22:23:57 +0400

    CARD-83: Verse: when showing 404 there's no way to navigate away from that page

--- app/scripts/controllers/verse404.js
+++ app/scripts/controllers/verse404.js
@@ -8,6 +8,9 @@
  * Controller of the literatorioApp
  */
 angular.module('literatorioApp')
-  .controller('Verse404Ctrl', function (Analytics) {
+  .controller('Verse404Ctrl', function (Analytics, $rootScope) {
     Analytics.trackEvent('web', 'verse404-init');
+
+    $rootScope.$broadcast('HeaderCtrl.doShow');
+    $rootScope.$broadcast('FooterCtrl.doShow');
   });
